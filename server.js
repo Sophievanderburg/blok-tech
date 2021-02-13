fec6e252f88452a68c57df8570948121c6f9f9f7
Sophievanderburg <sophievdburg9@gmail.com>
2021-02-13 12:23:18 +0100

    modified server: static files are public!!

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/static',express.static('public'));
+app.use(express.static('static/public'));
 app.get('/', onhome);
 app.get('/about', onabout);
 app.listen(3000);
