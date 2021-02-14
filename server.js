cf61345531d062daa2df0a9dd6b3619fa390db6e
Sophievanderburg <sophievdburg9@gmail.com>
2021-02-14 16:03:00 +0100

    create views folder and match.ejs & install ejs

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app = express();
 
+// view engine ejs
+//app.set('view engine', 'ejs');
+
+// make the static/public folder public
 app.use(express.static('static/public'));
+
+// make views/pages folder public
+app.use(express.static('views'));
+
+// server listens to port 3000
+app.listen(3000);
+
+// app.mehtod('path', callbackfunction)
 app.get('/', onhome);
 app.get('/about', onabout);
-app.listen(3000);
-app.use(error404)
+app.get('/match', onmatch);
 
+// errors
+app.use(error404);
+
+// callback functions
 function onhome (req,res) {
     res.send('<h1>Hello Client</h1>');
 }
 
 function onabout (req,res) {
     res.send('<h1>About page</h1>');
+}
+
+function onmatch (req, res) {
+    res.render('match.ejs');
 }
 
 function error404( req, res, next) {
