043db91df5fb5ce507d2c4e11e076dce1c99c4a3
Sophievanderburg <sophievdburg9@gmail.com>
2021-02-15 16:37:46 +0100

    delete index.html, modified server.js with Danny, nodemon script in package.json

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ const express = require('express');
 const app = express();
 
 // view engine ejs
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // make the static/public folder public
 app.use(express.static('static/public'));
-
-// make views/pages folder public
-app.use(express.static('views'));
-
-// server listens to port 3000
-app.listen(3000);
 
 // app.mehtod('path', callbackfunction)
 app.get('/', onhome);
@@ -21,6 +15,9 @@ app.get('/practice', onpractice);
 
 // errors
 app.use(error404);
+
+// server listens to port 3000
+app.listen(3000);
 
 // callback functions
 function onhome (req,res) {
