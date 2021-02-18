0f18cb7add1329a16ba2c92cad5d880eb22cb0c6
Sophievanderburg <sophievdburg9@gmail.com>
2021-02-18 12:27:47 +0100

    post to server with data.push

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //parses the data and stores it in req.body
+
+var data = [];
 
 // view engine ejs
 app.set('view engine', 'ejs');
@@ -8,11 +10,14 @@ app.set('view engine', 'ejs');
 // make the static/public folder public
 app.use(express.static('static/public'));
 
-// app.mehtod('path', callbackfunction)
+// bodyParser iets
+app.use(bodyParser.urlencoded({ extended: true}));
 
+// app.mehtod('path', callbackfunction)
 app.get('/', onsavedmatch);
 app.get('/match', onmatch);
 app.get('/practice', onpractice);
+app.post('/practice', postname);
 
 // errors
 app.use(error404);
@@ -30,7 +35,12 @@ function onmatch (req, res) {
 }
 
 function onpractice (req, res) {
-    res.render('practice.ejs', {name: 'Sophie', age: 19});
+    res.render('practice.ejs', {name:"Sophie", age: 19});
+}
+
+function postname (req, res){
+    data.push({firstname:req.body.firstname});
+    res.send("Je naam is " + req.body.firstname + ".");
 }
 
 function error404( req, res, next) {
