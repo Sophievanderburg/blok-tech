2366a507c5fa2320e85ac70bf428d724f8e83714
Sophievanderburg <sophievdburg9@gmail.com>
2021-03-08 09:06:30 +0100

    created a menu

--- static/public/script/scrips.js
+++ static/public/script/scrips.js
@@ -1,19 +1,40 @@
-// variables
-var removeButton = document.querySelector("main ul li > button");
-var popUp = document.querySelector("main article.popup");
-var buttonCancelPopup = document.querySelector("main article.popup button:last-of-type");
+"use strict";
+// JavaScript Document
+/*------------------- header menu laten zien ---------------------*/
+var kruisKnopje = document.querySelector("header nav > button");
 
-// eventhandlers
-function showPopup () {
-    console.log("show Pop-up");
-    popUp.classList.remove("invisible");
+var menuKnop = document.querySelector("header button");
+
+var menuSection = document.querySelector("header > nav");
+
+
+function toonSection() {
+    menuSection.classList.toggle("toonSection");
+    console.log("het werkt")
 }
 
-function hidePopup () {
-    console.log("hide Pop-up");
-    popUp.classList.add("invisible");
-}
+kruisKnopje.addEventListener('click', toonSection);
+menuKnop.addEventListener('click', toonSection);
 
-// eventlisteners
-removeButton.addEventListener('click', showPopup);
-buttonCancelPopup.addEventListener('click', hidePopup);
+
+// Pop-up nog werkend maken???
+
+// // variables
+// var removeButton = document.querySelector("main ul li > button");
+// var popUp = document.querySelector("main article.popup");
+// var buttonCancelPopup = document.querySelector("main article.popup button:last-of-type");
+
+// // eventhandlers
+// function showPopup () {
+//     console.log("show Pop-up");
+//     popUp.classList.remove("invisible");
+// }
+
+// function hidePopup () {
+//     console.log("hide Pop-up");
+//     popUp.classList.add("invisible");
+// }
+
+// // eventlisteners
+// //removeButton.addEventListener('click', showPopup);
+// //buttonCancelPopup.addEventListener('click', hidePopup);
