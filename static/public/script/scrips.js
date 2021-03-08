2335053dc8c28eb19f9e4b1434a954f62d2b43c2
Sophievanderburg <sophievdburg9@gmail.com>
2021-03-08 18:35:57 +0100

    changed confirm message

--- static/public/script/scrips.js
+++ static/public/script/scrips.js
@@ -17,7 +17,7 @@ var form = document.querySelectorAll("main ul li form, main article div form");
 
 for (var i=0; i < form.length; i++){
   form[i].addEventListener('submit', function(e) {
-    if (!window.confirm("Zekersteweten verwijderen???")) {
+    if (!window.confirm("Are you sure you want to delete this person?")) {
       e.preventDefault()
     }
   })
